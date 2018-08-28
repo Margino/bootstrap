74b7043c6a80c7b8643a6ef660203d6169783218
Alex Petrov <i@petrov.com.ru>
2018-08-28 22:33:46 +0300

    add ROOT

---
+++ root/base.js
@@ -0,0 +1,76 @@
+// Defer load CSS
+addScript('css/decor.min.css', 'link', 'screen');
+addScript('css/print.min.css', 'link', 'print');
+
+// Make the focus outline visible when using the tab key
+window.addEventListener('keydown', handleFirstTab);
+
+// Feedback
+console.warn('%c%s', 'font-weight: bold', 'Find a bug? Please let me know https://github.com/Margino/report-bugs');
+
+// Switch the site theme
+if (document.querySelector('#toggleTheme')) {
+    document.querySelector('#toggleTheme').addEventListener('click', () => {
+        document.body.classList.toggle('night');
+    });
+}
+
+
+// ========== Functions ========== //
+
+
+function handleFirstTab(event) {
+    if (event.keyCode === 9) {
+        document.body.classList.add('tabbing');
+
+        window.removeEventListener('keydown', handleFirstTab);
+        window.addEventListener('mousedown', handleMouseDownOnce);
+    }
+}
+
+function handleMouseDownOnce() {
+    document.body.classList.remove('tabbing');
+
+    window.removeEventListener('mousedown', handleMouseDownOnce);
+    window.addEventListener('keydown', handleFirstTab);
+}
+
+
+// Load scripts and styles
+function addScript(src, type, media) {
+
+    const element = document.createElement(type);
+
+    if (type === 'script') {
+        element.src = src;
+        element.charset = 'utf-8';
+        element.async = true;
+        document.body.appendChild(element);
+    } else if (type === 'link') {
+        element.rel = 'stylesheet';
+        element.href = src;
+        if (media !== undefined) {
+            element.media = media;
+        }
+        document.querySelector('head').appendChild(element);
+    }
+
+    // catch errors
+    element.onerror = function () {
+        const err = document.createElement('div');
+
+        err.textContent = `- The file «${src}» has not loaded. Try reloading the page.`;
+        // err.textContent = `— Файл «${src}» не загрузился. Попробуйте перезагрузить страницу.`;
+
+        document.querySelector('#attention').appendChild(err);
+
+        // Show the error block which is hidden in CSS by default
+        document.querySelector('#attention').style.display = 'block';
+    }
+}
+
+
+// Add a block after an element
+function insertAfter(elem, refElem) {
+      return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
+}
