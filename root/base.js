6537fcafb1b36155cb5873dbd3b7ad910c976599
Alex Petrov <i@petrov.com.ru>
2018-08-30 00:19:33 +0300

    fixed .attention list-style

--- root/base.js
+++ root/base.js
@@ -59,8 +59,8 @@ function addScript(src, type, media) {
     element.onerror = function () {
         const err = document.createElement('li');
 
-        err.textContent = `- The file «${src}» has not been loaded. Try reloading the page.`;
-        // err.textContent = `— Файл «${src}» не загрузился. Попробуйте перезагрузить страницу.`;
+        err.textContent = `The file «${src}» has not been loaded. Try reloading the page.`;
+        // err.textContent = `Файл «${src}» не загрузился. Попробуйте перезагрузить страницу.`;
 
         if (document.querySelector('#attention')) {
             document.querySelector('#attention').appendChild(err);
