38a85e25510a2533b0b9ba1bd84e08a2d1e71830
Alex Petrov <i@petrov.com.ru>
2018-09-06 19:31:14 +0300

    added img & frame onload check

--- youtube/youtube_lazy.js
+++ youtube/youtube_lazy.js
@@ -1,6 +1,9 @@
 const youtube = document.querySelectorAll('.youtube');
 
 youtube.forEach((item) => {
+
+    item.classList.add('load');
+
     const source = `https://img.youtube.com/vi/${item.dataset.embed}/sddefault.jpg`;
 
     // upload an image
@@ -10,13 +13,25 @@ youtube.forEach((item) => {
         item.appendChild(image);
     });
 
+    image.onload = () => {
+        item.classList.remove('load');
+    };
+
     item.querySelector('.youtube__btn').addEventListener('click', () => {
+
+        item.classList.add('load');
+
         const iframe = document.createElement('iframe');
         iframe.setAttribute('frameborder', '0');
         iframe.setAttribute('allowfullscreen', '');
         iframe.setAttribute('src', `https://www.youtube.com/embed/${item.dataset.embed}?rel=0&showinfo=0&autoplay=1`);
-
         item.innerHTML = '';
         item.appendChild(iframe);
+
+        iframe.onload = () => {
+            item.classList.remove('load');
+        };
+
+
     });
 });
