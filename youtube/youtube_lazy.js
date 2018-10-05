999bae22f414c227dcad06b5506db1f6a9ee710d
Alex Petrov <i@petrov.com.ru>
2018-10-05 17:32:05 +0300

    add fetures

--- youtube/youtube_lazy.js
+++ youtube/youtube_lazy.js
@@ -1,32 +1,51 @@
-const youtube = document.querySelectorAll('.youtube');
+(function () {
 
-youtube.forEach((item) => {
+    const youtube = document.querySelectorAll('.youtube');
+    const lang = getPageLanguage();
 
-    item.classList.add('load');
+    const youtubeText = {
+        button: {
+            ru: 'загрузить видео с YouTube',
+            en: 'show embed'
+        }
+    }
 
-    // upload an image
-    const image = new Image();
-    image.src = `https://img.youtube.com/vi/${item.dataset.embed}/sddefault.jpg`;
-    image.onload = () => {
-        item.appendChild(image);
-        item.classList.remove('load');
-    };
+    youtube.forEach((item) => {
 
-    item.querySelector('.youtube__btn').addEventListener('click', () => {
+        // add a play button
+        const playButton = document.createElement('button');
+        playButton.type = 'button';
+        playButton.classList.add('youtube__btn');
+        playButton.text = ${youtubeText.button[lang]};
+        console.log(playButton.text);
 
         item.classList.add('load');
 
-        const iframe = document.createElement('iframe');
-        iframe.setAttribute('frameborder', '0');
-        iframe.setAttribute('allowfullscreen', '');
-        iframe.setAttribute('src', `https://www.youtube.com/embed/${item.dataset.embed}?rel=0&showinfo=0&autoplay=1`);
-        item.innerHTML = '';
-        item.appendChild(iframe);
-
-        iframe.onload = () => {
+        // upload an image
+        const image = new Image();
+        image.src = `https://img.youtube.com/vi/${item.dataset.embed}/sddefault.jpg`;
+        image.onload = () => {
+            item.appendChild(image);
             item.classList.remove('load');
         };
 
+        item.querySelector('.youtube__btn').addEventListener('click', () => {
 
+            item.classList.add('load');
+
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('frameborder', '0');
+            iframe.setAttribute('allowfullscreen', '');
+            iframe.setAttribute('src', `https://www.youtube.com/embed/${item.dataset.embed}?rel=0&showinfo=0&autoplay=1`);
+            item.innerHTML = '';
+            item.appendChild(iframe);
+
+            iframe.onload = () => {
+                item.classList.remove('load');
+            };
+
+
+        });
     });
-});
+
+})();
